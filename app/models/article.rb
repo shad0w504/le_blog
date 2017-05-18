4c7f18576e091604e9bd85b11870982657596289
Chrono S <shad0w71245@gmail.com>
2017-05-18 23:30:54 +0000

    insert form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, pressence: true, length: { minimum: 3, maximum: 50}
-  validates :description, pressence: true, length: { minimum: 10, maximum: 500}
+  validates :title, presence: true, length: { minimum: 3, maximum: 50}
+  validates :description, presence: true, length: { minimum: 10, maximum: 500}
 end
