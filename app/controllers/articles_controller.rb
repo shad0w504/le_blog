946b628ebe23395d804b2ee30bb976c475469495
Chrono S <shad0w71245@gmail.com>
2017-05-19 21:51:25 +0000

    style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was Successfully created"
+      flash[:success] = "Article was Successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was Successfully updated"
+      flash[:success] = "Article was Successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     
-    flash[:notice] = "Article was Successfully deleted"
+    flash[:danger] = "Article was Successfully deleted"
     redirect_to articles_path
   end
    
