292a22e2b4bd08550ef081941289807e8ff50eb1
Chrono S <shad0w71245@gmail.com>
2017-05-22 20:41:59 +0000

    edit and form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = "Welcome to the family, #{@user.username}"
-      redirect_to root_path
+      redirect_to articles_path
     else
       render 'new'
     end
@@ -16,5 +16,19 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:username,:email,:password)
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Account successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 end
