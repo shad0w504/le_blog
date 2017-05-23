bf3e046e0d5e1311f3c03f3c84d060a58da801a6
Chrono S <shad0w71245@gmail.com>
2017-05-23 21:35:07 +0000

    testing 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'ChronoSL', email: "shad0w71245@gmail.com", password: "lol123", admin: true)
