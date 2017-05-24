50a48d1d44a9b6c1e49eb626e8acfd7b61adaa92
Chrono S <shad0w71245@gmail.com>
2017-05-24 18:32:04 +0000

    user restrictions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin_user, only: [:new, :create]
+  
   def new
     @category = Category.new
   end
@@ -24,5 +26,12 @@ class CategoriesController < ApplicationController
   private
     def category_params
       params.require(:category).permit(:name)
+    end
+    
+    def require_admin_user
+      if !logged_in? || (logged_in? and !current_user.admin?)
+        flash[:danger] = "Only the admin users can perform that action"
+        redirect_to categories_path
+      end
     end
 end
