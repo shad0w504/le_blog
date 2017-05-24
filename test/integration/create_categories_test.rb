e04be87b93edf7f3034caab3796e66be30bff8f2
Chrono S <shad0w71245@gmail.com>
2017-05-24 19:11:32 +0000

    test fix and fix navbar

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,21 +1,27 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(username: "pepito", email: "ex@example.com", password: "example", admin: true)
+  end
+  
   test "get new form and create category" do
+    sign_in_as(@user, "example")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
-        post_via_redirect categories_path, category: {name: "games"}
+      post_via_redirect categories_path, category: {name: "games"}
     end
     assert_template 'categories/index'
     assert_match "games", response.body
   end
   
   test "invalid category submission" do
+    sign_in_as(@user, "example")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
-        post categories_path, category: {name: ""}
+      post categories_path, category: {name: ""}
     end
     assert_template 'categories/new'
     assert_select 'h2.panel-title'
