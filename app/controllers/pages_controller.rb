d6ac5cb870fd33310e5fd1d7dced1003c84f4612
Chrono S <shad0w71245@gmail.com>
2017-05-26 20:53:01 +0000

    api test final 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,13 +15,24 @@ class PagesController < ApplicationController
   
   private
     def set_json
-      user = User.first
-      articles = user.articles
+      users = User.all
+      art = 0
       @json = Jbuilder.new
-      @json.set! :user do
-        @json.set! :name, user.username
+      @json.array! users do |u|
+        @json.set! :name, u.username
+        arts = u.articles
         @json.set! :articles do
-          @json.array! articles, :categories.name
+          @json.set! :categories do
+            @json.array! arts[0].categories do |c|
+              @json.title c.name
+              @json.set! :articles do
+                @json.array! c.articles do |a|
+                  @json.title a.title
+                  @json.description a.description
+                end
+              end
+            end
+          end
         end
       end
     end
