acb9d55c050579461c9189fc9131d5a5ecefc5f8
Chrono S <shad0w71245@gmail.com>
2017-06-01 17:59:39 +0000

    day x api testah 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,22 +16,19 @@ class PagesController < ApplicationController
   private
     def set_json
       users = User.all
+      categories = Category.all
       @json = Jbuilder.new
       @json.array! users do |u|
         @json.set! :name, u.username
         arts = u.articles
-        @json.set! :articles do
-          @json.set! :categories do
-            arts.each do |art|
-              @json.array! art.categories do |c|
-                @json.name c.name
-                @json.testah art
-                @json.set! :articles do
-                  @json.array! c.articles do |a|
-                    if (a.user == u)
-                      @json.title a.title
-                      @json.description a.description
-                    end
+        @json.set! :categories do
+          @json.array! categories do |c|
+            @json.set! c.name do
+              @json.set! :articles do
+                @json.array! c.articles do |a|
+                  if (a.user == u)
+                    @json.title a.title
+                    @json.description a.description
                   end
                 end
               end
@@ -39,9 +36,5 @@ class PagesController < ApplicationController
           end
         end
       end
-    end
-    
-    def current_user_article?(u, a)
-      u = a.user
     end
 end
